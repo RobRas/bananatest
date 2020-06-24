5c09b0efce7602e61b3062d78563dc07e165c9c8
Robert <bobbyras90@gmail.com>
2020-06-23 19:48:05 -0700

    Movement speed is now a per-direction constant. Gross.

--- banana game/scripts/Player.gd
+++ banana game/scripts/Player.gd
@@ -2,19 +2,17 @@ extends KinematicBody2D
 
 
 func _physics_process(delta: float):
-	var speed = 100
-
 	var x: float = 0
 	if Input.is_action_pressed("move_left"):
-		x -= 1
+		x -= 100
 	if Input.is_action_pressed("move_right"):
-		x += 1
+		x += 100
 	
 	var y: float = 0
 	if Input.is_action_pressed("move_up"):
-		y -= 1
+		y -= 100
 	if Input.is_action_pressed("move_down"):
-		y += 1
+		y += 100
 	
-	var velocity = Vector2(x, y) * speed
+	var velocity = Vector2(x, y)
 	move_and_slide(velocity)
